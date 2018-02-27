36f2feabf0c5e6f43cffbf60f42924c12b36ea16
ModeYapu <1207565967@qq.com>
2018-02-27 13:45:21 +0800

    swagger

--- netCoreweb/Startup.cs
+++ netCoreweb/Startup.cs
@@ -3,7 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using netCoreweb.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.IO;
 
 namespace netCoreweb
 {
@@ -21,6 +26,15 @@ namespace netCoreweb
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Hello", Version = "v1" });
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "netCoreweb.xml");
+                c.IncludeXmlComments(xmlPath);
+                c.OperationFilter<AddAuthTokenHeaderParameter>();
+            });
+            services.AddMvcCore().AddApiExplorer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +57,38 @@ namespace netCoreweb
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseSwagger(c =>
+            {
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.ShowExtensions();
+                c.ValidatorUrl(null);
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "test V1");
+            });
+        }
+    }
+    /// <summary>
+    /// this class is for swagger to generate AuthToken Header filed on swagger UI
+    /// </summary>
+    public class AddAuthTokenHeaderParameter : IOperationFilter
+    {
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<IParameter>();
+            }
+            operation.Parameters.Add(new NonBodyParameter()
+            {
+                Name = "token",
+                In = "header",
+                Type = "string",
+                Description = "token认证信息",
+                Required = true
             });
         }
     }
